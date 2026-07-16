6a486a6ed041e80f6b29ddeb16f8e91f75fcf1d3
amodgoel <45631028+amodgoel@users.noreply.github.com>
2026-07-16 12:16:06 +0530

    Refactor HTML structure in page-upload.php

--- admin/page-upload.php
+++ admin/page-upload.php
@@ -1,38 +1,64 @@
 <?php
+
 if ( ! defined( 'ABSPATH' ) ) {
-exit;
+
+	exit;
+
 }
+
 ?>
 <div class="wrap">
 <h1>Tarsh Product Uploader</h1>
 <p>
-       Bulk upload WooCommerce products with two images per product.
+
+		Bulk upload WooCommerce products with two images per product.
 </p>
 <hr>
 <table class="form-table">
 <tr>
-<th>Number of Products</th>
+<th scope="row">
+
+				Number of Products
+</th>
 <td>
 <input
-                   type="number"
-                   id="tpu-product-count"
-                   min="1"
-                   max="20"
-                   value="5">
+
+					type="number"
+
+					id="tpu-product-count"
+
+					min="1"
+
+					max="20"
+
+					value="5">
 <button
-                   class="button button-primary"
-                   id="tpu-create-products">
-                   Create Product Cards
+
+					type="button"
+
+					class="button button-primary"
+
+					id="tpu-create-products">
+
+					Create Product Cards
+</button>
+&nbsp;
+<button
+
+					type="button"
+
+					class="button"
+
+					id="tpu-upload-all">
+
+					Upload All Products
 </button>
 </td>
 </tr>
 </table>
-<br>
-<button
-       class="button"
-       id="tpu-upload-all">
-       Upload All Products
-</button>
 <hr>
-<div id="tpu-products-container"></div>
+<div id="tpu-products-container">
+<!-- Product cards will appear here -->
 </div>
+</div>
+ 
