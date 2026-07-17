7f089e157106b21e7cc012c1bed2d336f842f93e
amodgoel <45631028+amodgoel@users.noreply.github.com>
2026-07-17 09:04:46 +0530

    Update admin.js

--- admin/assets/js/admin.js
+++ admin/assets/js/admin.js
@@ -3,9 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Tarsh Product Uploader Started");
 
     const createButton = document.getElementById("tpu-create-products");
-
+    const uploadButton = document.getElementById("tpu-upload-all");
     const countInput = document.getElementById("tpu-product-count");
-
     const container = document.getElementById("tpu-products-container");
 
     createButton.addEventListener("click", function () {
@@ -23,12 +22,20 @@ document.addEventListener("DOMContentLoaded", function () {
         for (let i = 1; i <= total; i++) {
 
             container.appendChild(
-
                 TPUUI.createProductCard(i)
-
             );
 
         }
+
+    });
+
+    uploadButton.addEventListener("click", function () {
+
+        const products = TPUReader.readAllProducts();
+
+        console.clear();
+
+        console.table(products);
 
     });
 
